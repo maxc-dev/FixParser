76d57e15b01e09a8d7503617bf13b9b2aeeeca9c
maxc-dev <mer.cartermc@gmail.com>
2024-11-03 16:07:51 +0000

    added websocket support

---
+++ src/client.rs
@@ -0,0 +1,38 @@
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use futures_util::{SinkExt, StreamExt};
+use tokio::io::{self, AsyncBufReadExt};
+
+#[tokio::main]
+async fn main() {
+    let (mut socket, response) = connect_async("ws://127.0.0.1:8081/ws").await.expect("Failed to connect");
+
+    println!("Connected to WebSocket server [{}]. Type messages to send.", response.status());
+
+    let stdin = io::BufReader::new(io::stdin());
+    let mut lines = stdin.lines();
+
+    loop {
+        tokio::select! {
+            result = lines.next_line() => {
+                match result {
+                    Ok(Some(line)) => {
+                        socket.send(Message::Text(line)).await.expect("Failed to send message");
+                    }
+                    Ok(None) => break, // End of input
+                    Err(e) => {
+                        eprintln!("Error reading line: {}", e);
+                        break;
+                    }
+                }
+            },
+            Some(Ok(message)) = socket.next() => {
+                if let Message::Text(text) = message {
+                    println!("Received: {text}");
+                }
+            },
+            else => break,
+        }
+    }
+
+    println!("Disconnected from WebSocket server.");
+}
--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,21 @@ mod new_order;
 mod order_cancel_request;
 mod execution_report;
 mod order_status_request;
+mod client;
 
 use crate::execution_report::ExecutionReport;
 use crate::fix_message::FixMessage;
+use crate::fix_message_parser::FixMessageParser;
 use crate::new_order::NewOrder;
 use crate::order_cancel_request::OrderCancelRequest;
 use crate::order_status_request::OrderStatusRequest;
-use crate::fix_message_parser::FixMessageParser;
-use crate::FixMessageParsed::{Success, UnknownMessageType};
+use crate::FixMessageParseResponse::{Success, UnknownMessageType};
 
-use axum::{routing::post, Router};
+use axum::{routing::{post, get}, Router, {response::IntoResponse}, {extract::ws::{WebSocket, WebSocketUpgrade}}};
+use std::net::SocketAddr;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
-use std::net::SocketAddr;
+use futures_util::{SinkExt, StreamExt};
 
 const FIX_TAG: &str = "fix";
 
@@ -40,18 +42,29 @@ struct ApiDoc;
     )
 )]
 async fn post_fix_msg(body: String) -> String {
-    let result = match handle_fix_str(body.as_str()) {
-        Success => "Message parsed successfully.".to_string(),
-        UnknownMessageType => "Unknown message type received.".to_string()
-    };
+    let result = handle_fix_str(body.as_str());
     format!("Parse response: {:?}", result)
 
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_websocket)
+}
+
+async fn handle_websocket(mut socket: WebSocket) {
+    while let Some(Ok(message)) = socket.next().await {
+        if let axum::extract::ws::Message::Text(text) = message {
+            let parsed_message = handle_fix_str(&text);
+            socket.send(axum::extract::ws::Message::Text(format!("FIX Parser Response: {:?}", parsed_message))).await.unwrap();
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/api/v1/fix", post(post_fix_msg))
+        .route("/ws", get(ws_handler))
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8081));
@@ -63,40 +76,41 @@ async fn main() {
         .unwrap();
 }
 
-enum FixMessageParsed {
+#[derive(Debug)]
+enum FixMessageParseResponse {
     Success,
     UnknownMessageType,
 }
 
-fn handle_fix_str(msg: &str) -> FixMessageParsed {
+fn handle_fix_str(msg: &str) -> FixMessageParseResponse {
     match FixMessageParser::parse_message(msg) {
         FixMessage::NewOrder(new_order) => handle_new_order(new_order),
         FixMessage::ExecutionReport(execution_report) => handle_execution_report(execution_report),
         FixMessage::OrderCancelRequest(order_cancel_request) => handle_order_cancel_request(order_cancel_request),
         FixMessage::OrderStatusRequest(order_status_request) => handle_order_status_request(order_status_request),
-        FixMessage::Unknown => FixMessageParsed::UnknownMessageType
+        FixMessage::Unknown => UnknownMessageType
     }
 }
 
-fn handle_new_order(new_order: NewOrder) -> FixMessageParsed {
+fn handle_new_order(new_order: NewOrder) -> FixMessageParseResponse {
     println!("Handling NewOrder: {:?}", new_order.cl_ord_id);
     println!(" -> {:?}", new_order);
     Success
 }
 
-fn handle_execution_report(execution_report: ExecutionReport) -> FixMessageParsed {
+fn handle_execution_report(execution_report: ExecutionReport) -> FixMessageParseResponse {
     println!("Handling ExecutionReport: {:?}", execution_report.cl_ord_id);
     println!(" -> {:?}", execution_report);
     Success
 }
 
-fn handle_order_cancel_request(order_cancel_request: OrderCancelRequest) -> FixMessageParsed {
+fn handle_order_cancel_request(order_cancel_request: OrderCancelRequest) -> FixMessageParseResponse {
     println!("Handling OrderCancelRequest: {:?}", order_cancel_request.cl_ord_id);
     println!(" -> {:?}", order_cancel_request);
     Success
 }
 
-fn handle_order_status_request(order_status_request: OrderStatusRequest) -> FixMessageParsed {
+fn handle_order_status_request(order_status_request: OrderStatusRequest) -> FixMessageParseResponse {
     println!("Handling OrderStatusRequest: {:?}", order_status_request.cl_ord_id);
     println!(" -> {:?}", order_status_request);
     Success
